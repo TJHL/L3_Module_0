26d13856b9d4fc776b9de4c86f53db6b9e48a24f
TJHL <league@iMac.attlocal.net>
2017-09-17 17:26:43 -0700

    Guest Book Complete

--- src/IntroToArrayLists/GuestBook.java
+++ src/IntroToArrayLists/GuestBook.java
@@ -1,24 +1,29 @@
 package IntroToArrayLists;
+
 import javax.swing.JOptionPane;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 public class GuestBook implements ActionListener {
+	ArrayList<String> guests = new ArrayList<String>();
 	String input;
 	JPanel panel = new JPanel();
 	JFrame frame = new JFrame();
 	JButton addButton = new JButton("Add Name");
 	JButton viewButton = new JButton("View Names");
 
-	public static void main(String[] args)  {
+	public static void main(String[] args) {
 		GuestBook launch = new GuestBook();
+
 	}
-	GuestBook(){
+
+	GuestBook() {
 		frame.add(panel);
 		panel.add(addButton);
 		panel.add(viewButton);
@@ -28,20 +33,22 @@ public class GuestBook implements ActionListener {
 		frame.pack();
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
-	
+
 	public void actionPerformed(ActionEvent e) {
-		if(e.getSource()==addButton){
-		input=JOptionPane.showInputDialog("Enter A name");
-		System.out.println(input);
+		
+		if (e.getSource() == addButton) {
+			input = JOptionPane.showInputDialog("Enter A name");
+			guests.add(input + "\n");
+
+		}
+		
+		if (e.getSource() == viewButton) {
+			String guestPosition= "";
+			for (int i = 0; i < guests.size(); i++) {
+			guestPosition+="Guest #"+(i+1)+": "+guests.get(i);
+				
+			}
+			JOptionPane.showMessageDialog(null, "Guests \n" + guestPosition);
 		}
 	}
-		
-	// Create a GUI with two buttons. One button reads "Add Name" and the other button reads "View Names". 
-	// When the add name button is clicked, display an input dialog that asks the user to enter a name. Add
-	// that name to an ArrayList. When the "View Names" button is clicked, display a message dialog that displays
-	// all the names added to the list. Format the list as follows:
-	// Guest #1: Bob Banders
-	// Guest #2: Sandy Summers
-	// Guest #3: Greg Ganders
-	// Guest #4: Donny Doners
 }
