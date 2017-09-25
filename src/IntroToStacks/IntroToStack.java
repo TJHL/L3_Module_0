3cf21169d0d01a2b8b17f18bdc7a05292e3f16b9
TJHL <league@iMac.local>
2017-09-24 17:27:17 -0700

    Intro stacks
    kinda complete, getting  errors on return

--- src/IntroToStacks/IntroToStack.java
+++ src/IntroToStacks/IntroToStack.java
@@ -1,13 +1,46 @@
 package IntroToStacks;
 
+import java.util.Random;
+import java.util.Stack;
+
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
 public class IntroToStack {
+	
+	JFrame frame = new JFrame();
+	JPanel panel= new JPanel();
+	Random a= new Random();
+	Double less;
+	Double more;
+	Stack<Double> num = new Stack<Double>();
+	
 	public static void main(String[] args) {
-		//1. Create a Stack of Doubles
-		//   Don't forget to import the Stack class
 		
-		//2. Use a loop to push 100 random doubles between 0 and 100 to the Stack.
-		
-		//3. Ask the user to enter in two numbers between 0 and 100, inclusive. 
+		IntroToStack s =new IntroToStack();
+		s.numberOperating();
+	}
+		IntroToStack() {
+			frame.add(panel);
+			
+		}
+		void numberOperating(){
+			//3. Ask the user to enter in two numbers between 0 and 100, inclusive. 
+			less=Double.parseDouble(JOptionPane.showInputDialog("Helllo"));
+			more=Double.parseDouble(JOptionPane.showInputDialog("Helllo too"));
+			
+			for (int i = 0; i < 100; i++) {
+				num.push((a.nextDouble())*100);
+				
+			}
+			for (int i = 0; i < 100; i++) {
+				//num.pop();
+				if (less<= num.pop() && num.pop() <=more) {
+				System.out.println(num.pop());
+				}
+			}
+		}
 		
 		//4. Pop all the elements off of the Stack. Every time a double is popped that is
 		//   between the two numbers entered by the user, print it to the screen.
@@ -25,5 +58,5 @@ public class IntroToStack {
 		//   71.54506465
 		//   66.47984807
 		//   74.12121224
-	}
+	
 }
