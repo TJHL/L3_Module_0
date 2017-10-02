948ed097f8f4fd911ba74aa2f8eeee51832bb2cc
TJHL <league@iMac.attlocal.net>
2017-10-01 17:29:52 -0700

    Undo redo
    Enter does not work.

--- src/IntroToStacks/IntroToStack.java
+++ src/IntroToStacks/IntroToStack.java
@@ -2,7 +2,6 @@ package IntroToStacks;
 
 import java.util.Random;
 import java.util.Stack;
-
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
@@ -26,37 +25,20 @@ public class IntroToStack {
 			
 		}
 		void numberOperating(){
-			//3. Ask the user to enter in two numbers between 0 and 100, inclusive. 
-			less=Double.parseDouble(JOptionPane.showInputDialog("Helllo"));
-			more=Double.parseDouble(JOptionPane.showInputDialog("Helllo too"));
+			less=Double.parseDouble(JOptionPane.showInputDialog("Enter a number between 0 and 100"));
+			more=Double.parseDouble(JOptionPane.showInputDialog("Enter a number greater than your first number"));
 			
 			for (int i = 0; i < 100; i++) {
 				num.push((a.nextDouble())*100);
 				
 			}
+			
 			for (int i = 0; i < 100; i++) {
-				//num.pop();
-				if (less<= num.pop() && num.pop() <=more) {
-				System.out.println(num.pop());
+				double d=num.pop();
+				if (less<= d && d <=more) {
+				System.out.println(d);
 				}
 			}
 		}
-		
-		//4. Pop all the elements off of the Stack. Every time a double is popped that is
-		//   between the two numbers entered by the user, print it to the screen.
-		
-		//   EXAMPLE:
-		//   NUM 1: 65
-		//   NUM 2: 75
-		
-		//   Popping elements off stack...
-		//   Elements between 65 and 75:
-		//   66.66876846
-		//   74.51651681
-		//   70.05110654
-		//   69.21350456
-		//   71.54506465
-		//   66.47984807
-		//   74.12121224
 	
 }
--- src/IntroToStacks/TextUndoRedo.java
+++ src/IntroToStacks/TextUndoRedo.java
@@ -1,6 +1,29 @@
 package IntroToStacks;
 
-public class TextUndoRedo {
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class TextUndoRedo implements KeyListener {
+
+	JFrame frame = new JFrame();
+	JPanel panel = new JPanel();
+	JLabel label = new JLabel("Suck lead, not actually commusist scum!");
+	public static void main(String[] args) {
+		TextUndoRedo a= new TextUndoRedo();
+	}
+	TextUndoRedo(){
+		frame.add(panel);
+		panel.add(label);
+		frame.addKeyListener(this);
+		frame.setSize(500, 500);
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+	}
 	/* 
 	 * Create a JFrame with a JPanel and a JLabel.
 	 * 
@@ -13,5 +36,18 @@ public class TextUndoRedo {
 	 * off the Stack and added back to the JLabel.
 	 * 
 	 * */
+	
+	
+	public void keyPressed(KeyEvent e) {
+		if(e.getKeyCode()==KeyEvent.VK_ENTER) {
+			label.setText(label.getText()+"\n");
+		}
+		label.setText(label.getText()+e.getKeyChar());
+			
+		
+		
+	}
+	
+	public void keyReleased(KeyEvent e){}public void keyTyped(KeyEvent e) {}
 	
 }
