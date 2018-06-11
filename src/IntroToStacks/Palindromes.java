885bb5533ede66d663743a9240344a3c19173fd0
TJHL <league@iMac-2.attlocal.net>
2018-06-10 17:20:23 -0700

    Palindromes Finished
    START THE GODDAMN PREACtiC E tEST

--- src/IntroToStacks/Palindromes.java
+++ src/IntroToStacks/Palindromes.java
@@ -5,20 +5,34 @@ import java.util.Stack;
 public class Palindromes {
 
 	boolean isPalindrome(String possiblePalindrome) {
-		Stack palNum = new Stack();
-		String palTest = new String();
+		Stack <Character> palNumber = new Stack <Character>();
+		String palTest = "";
+		String simpPal = "";
 
+		
 		for (int i = 0; i < possiblePalindrome.length(); i++) {
-
-			palNum.push(possiblePalindrome.charAt(i));
+			char c = possiblePalindrome.charAt(i);
+			if(c==' '||c==','||c=='.'||c=='?'||c=='!') {
+				
+			}else {
+				char f = Character.toLowerCase(c);
+				simpPal+=(f);
+			}
 		}
-		for (int i = 0; i < possiblePalindrome.length(); i++) {
-			palTest = palTest + palNum.pop();
+		for (int i = 0; i < simpPal.length(); i++) {
+			palNumber.push(simpPal.charAt(i));
 		}
-		if (palTest == possiblePalindrome) {
+		
+		for (int i = 0; i < simpPal.length(); i++) {
+			if(!palNumber.isEmpty()) {
+			palTest = palTest + palNumber.pop();}
+		}
+		System.out.println(palTest);
+		System.out.println(simpPal);
+		if (palTest.equals(simpPal)) {
 			return true;
 		} else
-			;
+		
 		return false;
 	}
 }
